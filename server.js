90dbe3b5f0aa19fe71e14825892dff9f58773f9e
dlhardi <derricklhardison@gmail.com>
2021-03-02 13:06:42 -0500

    Set server PORT to 3001, listening on the port, created instance of express, added middleware, added test route

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/api/config", (req, res) => {
+    res.json({
+      success: true,
+      
+    });
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
