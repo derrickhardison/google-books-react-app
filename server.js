d4a1ba41f34c0e954080e7fa49ed0fe1c2bb80c0
dlhardi <derricklhardison@gmail.com>
2021-03-02 13:49:55 -0500

    Added express static to serve up build folder

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("client/build"));
 app.use(express.json());
 app.use(routes);
+
 
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/reactgooglebooks", {
