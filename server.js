5ac9e07a96bafbad26d832ca2e7813720e2c4901
dlhardi <derricklhardison@gmail.com>
2021-03-02 13:16:50 -0500

    Added mongoDB and mongoose to the server

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(routes);
 
-app.get("/api/config", (req, res) => {
-    res.json({
-      success: true,
-      
-    });
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/reactgooglebooks", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
   });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
