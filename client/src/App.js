1b86764bf3da0219fb4e6ec305da034db15adffb
dlhardi <derricklhardison@gmail.com>
2021-03-02 15:21:17 -0500

    Imported components and tools from react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
+// MODULE IMPOPRTS
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
+
+// COMPONENT IMPORTS
+import Header from "./components/Header/Header"
+import Hero from "./components/Hero/Hero"
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
+import Footer from "./components/Footer/Footer"
 
 
 function App() {
   return (
     <div className="App">
-  <h1>Hello World</h1>
+   <Header />
+   <Hero />
+   <Router>
+     <Switch>
+     <Route exact path = "/" component = {Search} />
+     <Route exact path = "/" component = {Saved} />
+     </Switch>
+   </Router>
+   <Footer />
     </div>
   );
 }
