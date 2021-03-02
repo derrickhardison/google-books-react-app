0b21b1a8bc506fff85826a7f4df2106ce2d333a6
dlhardi <derricklhardison@gmail.com>
2021-03-02 13:13:48 -0500

    Required routes. Set express to use routes.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(routes);
 
 app.get("/api/config", (req, res) => {
     res.json({
