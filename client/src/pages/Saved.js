9e297451af94315c0b590a77169a24d656eb1617
dlhardi <derricklhardison@gmail.com>
2021-03-02 18:33:33 -0500

    Imported useState and useEffect from react. Set up axios call to get all saved books inside of useEffect hook

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import API from "../utils/API";
 import SavedBooks from "../components/SavedBooks/SavedBooks";
+import axios from "axios";
 
 const Saved = () => {
   // Deletes a book from the database with a given id, then reloads books from the db
@@ -9,6 +10,13 @@ const Saved = () => {
   //     .then(res => console.log(res))
   //     .catch(err => console.log(err));
   // }
+  useEffect(()=> {
+    
+    axios.get("/api/books").then((response) => {
+      console.log(response);
+    });
+  })
+
   return (
     <div className="container">
       <div className="row">
