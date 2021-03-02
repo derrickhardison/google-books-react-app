7a1b821bdf10f9919a496dbd91521f81a05bd3b3
dlhardi <derricklhardison@gmail.com>
2021-03-02 18:06:16 -0500

    Modified data type for authors so that multiple authors can be stored when needed

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  authors: { type: String, required: true },
+  authors: { type: Array, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
   link: { type: String, required: true },
